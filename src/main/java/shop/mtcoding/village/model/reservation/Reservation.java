047c80bf973924d62a85d4cc78026ea3e93523d3
khh5762 <khh5762@naver.com>
2023-04-25 11:16:09 +0900

    rebase 8번째

--- src/main/java/shop/mtcoding/village/model/reservation/Reservation.java
+++ src/main/java/shop/mtcoding/village/model/reservation/Reservation.java
@@ -5,24 +5,19 @@ import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import shop.mtcoding.village.core.jpa.BaseTime;
-import shop.mtcoding.village.dto.reservation.ReservationDTO;
-import shop.mtcoding.village.dto.reservation.response.ReservationSaveResponse;
 import shop.mtcoding.village.model.place.Place;
 import shop.mtcoding.village.model.user.User;
-import shop.mtcoding.village.util.TotalPrice;
 import shop.mtcoding.village.util.status.ReservationStatus;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@ToString
 @Table(name = "reservation_tb")
-public class Reservation {
+public class Reservation extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,12 +26,12 @@ public class Reservation {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @Comment("예약한 유저 정보")
-    @JoinColumn(name = "user_id")
     private User user;
 
     @Comment("공간 정보")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "place_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Place place;
 
     @Comment("예약 날짜")
@@ -55,8 +50,6 @@ public class Reservation {
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
 
-
-
     @Builder
     public Reservation(User user, Place place, LocalDateTime date, LocalDateTime startTime, LocalDateTime endTime, Integer peopleNum, ReservationStatus status) {
         this.user = user;
@@ -67,4 +60,45 @@ public class Reservation {
         this.peopleNum = peopleNum;
         this.status = status;
     }
+
+    public Reservation(User user, LocalDateTime date, LocalDateTime startTime, LocalDateTime endTime, Integer peopleNum) {
+        this.user = user;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.peopleNum = peopleNum;
+    }
+
+    public ReservationSaveResponse toResponse() {
+        User userName = new User();
+        userName.setName(user.getName());
+
+        return new ReservationSaveResponse(userName, peopleNum, date, startTime, endTime);
+    }
+//
+//    public ReservationDTO toDTOResponse() {
+//
+//        User userBuild = User.builder().build();
+//
+//        Place placeBuild = Place.builder().build();
+//
+//        Integer totalPrice = TotalPrice.calculateTotalPrice(Reservation.builder().build());
+//
+//        return new ReservationDTO(userBuild, placeBuild, peopleNum, totalPrice, date, startTime, endTime);
+//    }
+//
+    public ReservationDTO toDTOResponse() {
+        Integer totalPrice = TotalPrice.calculateTotalPrice(this);
+        System.out.println("디버그 : " + totalPrice);
+        return new ReservationDTO(
+                this.getUser(),
+                this.getPlace(),
+                this.getPeopleNum(),
+                totalPrice,
+                this.getDate(),
+                this.getStartTime(),
+                this.getEndTime()
+        );
+    }
+
 }
