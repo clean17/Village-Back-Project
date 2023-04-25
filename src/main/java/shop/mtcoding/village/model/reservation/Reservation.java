77da7d75b73f59bb31d80b9fda3d06ea5dbdbc78
khh5762 <khh5762@naver.com>
2023-04-25 11:11:43 +0900

    rebase 2번째

--- src/main/java/shop/mtcoding/village/model/reservation/Reservation.java
+++ src/main/java/shop/mtcoding/village/model/reservation/Reservation.java
@@ -29,7 +29,7 @@ public class Reservation {
     @Comment("예약 아이디")
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(fetch = FetchType.LAZY)
     @Comment("예약한 유저 정보")
     @JoinColumn(name = "user_id")
     private User user;
@@ -55,6 +55,8 @@ public class Reservation {
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
 
+
+
     @Builder
     public Reservation(User user, Place place, LocalDateTime date, LocalDateTime startTime, LocalDateTime endTime, Integer peopleNum, ReservationStatus status) {
         this.user = user;
@@ -65,46 +67,4 @@ public class Reservation {
         this.peopleNum = peopleNum;
         this.status = status;
     }
-
-    public Reservation(User user, LocalDateTime date, LocalDateTime startTime, LocalDateTime endTime, Integer peopleNum, ReservationStatus status) {
-        this.user = user;
-        this.date = date;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.peopleNum = peopleNum;
-        this.status = status;
-    }
-
-    public ReservationSaveResponse toResponse() {
-        User userName = new User();
-        userName.setName(user.getName());
-
-        return new ReservationSaveResponse(userName, peopleNum, date, startTime, endTime, status);
-    }
-//
-//    public ReservationDTO toDTOResponse() {
-//
-//        User userBuild = User.builder().build();
-//
-//        Place placeBuild = Place.builder().build();
-//
-//        Integer totalPrice = TotalPrice.calculateTotalPrice(Reservation.builder().build());
-//
-//        return new ReservationDTO(userBuild, placeBuild, peopleNum, totalPrice, date, startTime, endTime);
-//    }
-//
-    public ReservationDTO toDTOResponse() {
-        Integer totalPrice = TotalPrice.calculateTotalPrice(this);
-        System.out.println("디버그 : " + totalPrice);
-        return new ReservationDTO(
-                this.getUser(),
-                this.getPlace(),
-                this.getPeopleNum(),
-                totalPrice,
-                this.getDate(),
-                this.getStartTime(),
-                this.getEndTime()
-        );
-    }
-
 }
