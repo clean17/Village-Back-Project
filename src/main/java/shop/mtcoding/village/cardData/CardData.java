ebfc6cfdc6adbfb6830e287b40810889a0c95aa0
khh5762 <khh5762@naver.com>
2023-04-26 12:38:31 +0900

    결제 내역 받아보기

---
+++ src/main/java/shop/mtcoding/village/cardData/CardData.java
@@ -0,0 +1,37 @@
+//package shop.mtcoding.village.cardData;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Getter
+//@Setter
+//@Table(name = "card_data_tb")
+//public class CardData {
+//
+//    @Id
+//    private String tid;
+//
+//    @Column(name = "card_approve_no")
+//    private String cardApproveNo;
+//
+//    @Column(name = "card_no")
+//    private String cardNo;
+//
+//    @Column(name = "card_quota")
+//    private String cardQuota;
+//
+//    @Column(name = "card_company_code")
+//    private String cardCompanyCode;
+//
+//    @Column(name = "card_company")
+//    private String cardCompany;
+//
+//
+//
+//}
