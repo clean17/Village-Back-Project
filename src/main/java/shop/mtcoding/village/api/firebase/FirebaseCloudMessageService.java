3a32e7b201da1df6a2cbb555e21108558a0a410b
khh5762 <khh5762@naver.com>
2023-05-15 14:38:51 +0900

    배포 준비

--- src/main/java/shop/mtcoding/village/api/firebase/FirebaseCloudMessageService.java
+++ src/main/java/shop/mtcoding/village/api/firebase/FirebaseCloudMessageService.java
@@ -1,5 +1,9 @@
 package shop.mtcoding.village.api.firebase;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -9,8 +13,13 @@ import okhttp3.*;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 @Component
 @RequiredArgsConstructor
@@ -25,15 +34,17 @@ public class FirebaseCloudMessageService {
 
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        System.out.println("firebase1111111111111");
         Request request = new Request.Builder()
                 .url(API_URL)
                 .post(requestBody)
                 .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
                 .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
                 .build();
-
+        System.out.println("444444444444444444444444444444444444");
         Response response = client.newCall(request)
                 .execute();
+        System.out.println("5555555555555555555555555555555");
 
     }
 
@@ -56,15 +67,75 @@ public class FirebaseCloudMessageService {
     }
 
     private String getAccessToken() throws IOException {
-        String firebaseConfigPath = "/firebase/fcmgood-41ac1-f214841e94d2.json";
+        String bucketName = "villages-3";
+        String objectKey = "fcmgood-41ac1-f214841e94d2.json";
+        S3Object s3Object = null;
 
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
+            s3Object = s3Client.getObject(bucketName, objectKey);
+        } catch (AmazonServiceException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
 
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
-                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+        try {
+            InputStream inputStream = s3Object.getObjectContent();
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(inputStream)
+                    .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+            googleCredentials.refreshIfExpired();
+            return googleCredentials.getAccessToken().getTokenValue();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.exit(1);
+            return "";
+        }
 
-        googleCredentials.refreshIfExpired();
-        return googleCredentials.getAccessToken().getTokenValue();
+//        System.out.println("1111111111111111111111111111111111");
+//        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+//                .withRegion(Regions.AP_NORTHEAST_2) // 리전 설정
+//                .build();
+//        S3Object o = s3.getObject("villages-3", "fcmgood-41ac1-f214841e94d2.json");
+//        System.out.println("2222222222222222222222222222222222");
+//        S3ObjectInputStream s3is = o.getObjectContent();
+//        System.out.println("333333333333333333333333333");
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(s3is));
+//        String line;
+//        StringBuffer sb = new StringBuffer();
+//
+//        while((line = reader.readLine()) != null) {
+//            System.out.println("디버그 line : "+line);
+//            sb.append(line);
+//        }
+//
+//        reader.close();
+//        s3is.close();
+////        String firebaseConfigPath = sb.toString();
+//        String firebaseConfigPath = sb.toString();
+//
+//
+//        System.out.println("디버그 최종 : " +firebaseConfigPath);
+//
+//        try {
+//            FileWriter fileWriter = new FileWriter("src/main/resources/firebase/path.json");
+//            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//            bufferedWriter.write(firebaseConfigPath);
+//            bufferedWriter.close();
+//            System.out.println("디버그 try");
+//        } catch (IOException e) {
+//            System.out.println("디버그 catch");
+//            e.printStackTrace();
+//        }
+//
+//        String firebasePath = "/firebase/path.json";
+//
+//
+//        GoogleCredentials googleCredentials = GoogleCredentials
+//                .fromStream(new ClassPathResource(firebasePath).getInputStream())
+//                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+//
+//        googleCredentials.refreshIfExpired();
+//        return googleCredentials.getAccessToken().getTokenValue();
     }
 
 }
